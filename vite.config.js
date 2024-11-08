8725ea64afd8c268aa3b1e8220d5d46841b4a234
Lalit3572 <codefactory.jsx@gmail.com>
2024-11-08 13:16:04 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import { defineConfig } from "vite"
  
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // This allows Vite to bind to 0.0.0.0
+    port: 5173        // Optional: specify a port if needed (e.g., 3000)
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
